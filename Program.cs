9f1014ea6fe0ada0a51b56d6d4d8e4537880712d
Chamal Senarathna <chamals004@gmail.com>
2026-08-28 22:06:28 +0530

    urls

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var urls = builder.Configuration["ASPNETCORE_URLS"] ?? "http://0.0.0.0:8080";
+builder.WebHost.UseUrls(urls);
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
